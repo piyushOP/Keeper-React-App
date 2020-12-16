4083c2be9db73404b5efb4badf9501c266e481d8
piyushOP <pk930558@gmail.com>
2020-12-16 19:33:29 +0530

    Add some more components to project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,31 +4,38 @@ import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
-function App() {
 
+function App() {
   const [notes, setNotes] = useState([]);
 
   function addNote(newNote) {
-    setNotes( (prevNotes)=> {
+    setNotes(prevNotes => {
       return [...prevNotes, newNote];
     });
-  };
+  }
 
-  function deleteNote(id){
+  function deleteNote(id) {
     setNotes(prevNotes => {
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
-      })
-    })
+      });
+    });
   }
-
 
   return (
     <div>
       <Header />
-      <CreateArea onAdd={addNote}/>
+      <CreateArea onAdd={addNote} />
       {notes.map((noteItem, index) => {
-        return <Note id={index} key={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}/>
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
       <Footer />
     </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,37 +1,63 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
 
 function CreateArea(props) {
+
+  const [isExpended, setExpended] = useState(false);
 
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
 
-  function handleChange(event){
-    const {name, value} = event.target;
-    setNote( (prevNote) => {
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
       return {
         ...prevNote,
         [name]: value
       };
     });
-  };
+  }
 
-  function submitNote(event){
+  function submitNote(event) {
     props.onAdd(note);
     setNote({
       title: "",
       content: ""
     });
-    return event.preventDefault();
+    event.preventDefault();
+  }
+
+  function expand(){
+    setExpended(true);
   }
 
   return (
     <div>
-      <form>
-        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
-        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
-        <button onClick={submitNote} >Add</button>
+      <form className="create-note">
+        {isExpended && 
+          <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />}
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpended ? "3" : "1"}
+        />
+        <Zoom in={isExpended}>
+        <Fab onClick={submitNote}><AddIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
